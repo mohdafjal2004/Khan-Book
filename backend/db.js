bc5811b55f3f1ccee7ff4dd58b04de9afb98639e
mohdafjal2004 <86000714+mohdafjal2004@users.noreply.github.com>
2022-03-25 11:56:24 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const conn = () => {
-  mongoose.connect("mongodb+srv://codewithharry:codewithharry@cluster0.2cdxj.mongodb.net/harry?retryWrites=true&w=majority")
+  mongoose.connect("mongodb+srv://<username>:<password>@cluster0.2cdxj.mongodb.net/harry?retryWrites=true&w=majority")
     .then(() => {
       console.log("Connected to DB");
     })
